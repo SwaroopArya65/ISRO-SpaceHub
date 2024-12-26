a229afa3a54f82c2d5745237b575053f0561071c
Arya Swaroop <aryaswaroop027@gmail.com>
2024-12-26 19:12:43 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Index from './pages/Index'
+
+function App() {
+  return (
+    <>
+      <div className="text-xl font-bold underline">
+        <Index />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './../components/Navbar';
+import Footer from './../components/Footer';
+
+const Index = () => {
+    return (
+        <div>
+            Main web page goes here....
+        </div>
+    )
+}
+
+export default Index;
