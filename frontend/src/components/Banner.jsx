dd56e53dd08f56c6f63599b2fea499e239c773a6
Arya Swaroop <aryaswaroop027@gmail.com>
2024-12-27 22:47:08 +0530

    Adding some component

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div>
+            This page goes to Banner section....
+        </div>
+    )
+}
+
+export default Banner;
