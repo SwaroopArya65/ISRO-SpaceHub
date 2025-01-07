af526f3ae46ba6a4c9d34a642ca5b98935501668
Arya Swaroop <aryaswaroop027@gmail.com>
2025-01-07 18:32:59 +0530

    Almost complete login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            Goes to user login here....
+        <div className="bg-[url('./assets/bg.jpg')] bg-no-repeat bg-cover">
+            <div className="h-screen flex justify-center items-center">
+                <div className="bg-white mx-4 p-8 rounded shadow-md w-full md:w-1/2 lg:w-1/3">
+                    <h1 className="text-3xl font-playfair tracking-wider font-bold mb-8 text-center">Login Account</h1>
+                    <form>
+                        <div className="mb-4">
+                            <label className="block font-publicSans tracking-wide font-semibold text-slate-900 mb-2" for="email">
+                                Email Address
+                            </label>
+                            <input
+                                className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline font-publicSans"
+                                id="email" type="email" placeholder="Enter your email address" />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block font-publicSans tracking-wide font-semibold text-slate-900 mb-2" for="password">
+                                Password
+                            </label>
+                            <input
+                                className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline font-publicSans"
+                                id="password" type="password" placeholder="Enter your password" />
+                            <a className="text-gray-600 hover:text-gray-800 font-publicSans tracking-wide" href="#">Forgot your password?</a>
+                        </div>
+                        <div className="mb-6">
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline font-playfair tracking-wider"
+                                type="button">
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
