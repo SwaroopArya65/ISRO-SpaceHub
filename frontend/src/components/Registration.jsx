86aea9667af253db6339167c9160f616bb34a2d5
Arya Swaroop <aryaswaroop027@gmail.com>
2025-01-08 21:51:42 +0530

    Add new component and still modifying the login component

---
+++ frontend/src/components/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Registration = () => {
+    return (
+        <div>
+            Registration UI goes here....
+        </div>
+    )
+}
+
+export default Registration;
