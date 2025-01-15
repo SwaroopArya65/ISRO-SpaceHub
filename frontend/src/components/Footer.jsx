309e7b3d0b62c112a269b3cf99aad5e3f3764e08
Arya Swaroop <aryaswaroop027@gmail.com>
2025-01-15 21:42:05 +0530

    same thinks also change in

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,12 +11,12 @@ const Footer = () => {
                     aryaswaroop027@gmail.com
                 </div>
             </div> */}
-            <div class="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
-                <p>CopyRight Recived 2025 Arya swaroop. All rights reserved.</p>
+            <div class="text-center sm:flex items-center justify-between border-t border-text-color2 mx-[10%] mt-12 py-6">
+                <p className='font-publicSans text-text-color tracking-wide font-semibold'>CopyRight Recived 2025 Arya swaroop. All rights reserved.</p>
                 <ul class="flex items-center gap-5 justify-center mt-4 sm:mt-0">
-                    <li><a href="https://github.com/SwaroopArya65">GitHub</a></li>
-                    <li><a href="https://www.linkedin.com/in/arya-swaroop-0a313b1b2">LinkedIn</a></li>
-                    <li><a href="#">Quick Link</a></li>
+                    <li><a href="https://github.com/SwaroopArya65" className='font-publicSans text-text-color tracking-wide font-semibold'>GitHub</a></li>
+                    <li><a href="https://www.linkedin.com/in/arya-swaroop-0a313b1b2" className='font-publicSans text-text-color tracking-wide font-semibold'>LinkedIn</a></li>
+                    <li><a href="#" className='font-publicSans text-text-color tracking-wide font-semibold'>Quick Link</a></li>
                 </ul>
             </div>
         </div>
