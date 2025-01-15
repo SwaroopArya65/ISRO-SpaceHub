721732e47a3e7dd2b99ee7285ade0cbd4f965c4c
Arya Swaroop <aryaswaroop027@gmail.com>
2025-01-15 21:28:19 +0530

    change the contact page desgin

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -8,29 +8,29 @@ import contact from '../assets/s1.jpg';
 const ContactUs = () => {
     return (
         // relative isolate px-6 pt-14 pb-14 lg:px-40 bg-zinc-50
-        <div className="relative isolate px-6 pt-14 pb-10 lg:px-40 aspect-square bg-[url('./assets/1.png')] bg-no-repeat bg-contain group h-[400px] w-full">
+        <div className="relative isolate px-6 pt-14 pb-10 lg:px-40 aspect-square bg-[url('./assets/cb.png')] bg-no-repeat bg-contain group h-[400px] w-full">
             <GradientBg position="top-[calc(100%-13rem)] sm:top-[calc(100%-40rem)]" />
             <div className='flex items-center justify-between'>
                 <div>
-                    <blockquote class="text-4xl tracking-wider font-semibold font-playfair text-start text-slate-900 mb-5">
+                    <blockquote class="text-4xl tracking-wider font-bold font-playfair text-start text-text-color2 mb-5">
                         Get In
                         <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-bg relative inline-block ml-2">
                             <span class="relative text-white italic roun">Touch</span>
                         </span>
                     </blockquote>
                     <div className='flex items-center mb-2'>
-                        <IoLocationSharp className='mr-3 text-xl' />
-                        <span className='text-md font-publicSans tracking-wider font-medium text-gray-900'>Mangal Murti Apartment <br /> Tilker nagar, Hadapsar <br /> pune, Maharastra 411002</span>
+                        <IoLocationSharp className='mr-3 text-xl text-text-color' />
+                        <span className='text-md font-publicSans tracking-wider font-semibold text-text-color'>Mangal Murti Apartment <br /> Tilker nagar, Hadapsar <br /> pune, Maharastra 411002</span>
                     </div>
                     <div className='flex items-center mb-2'>
-                        <MdMarkEmailRead className='mr-3 text-xl' />
-                        <span className='text-md font-publicSans tracking-wider font-medium text-gray-900'>
+                        <MdMarkEmailRead className='mr-3 text-xl text-text-color' />
+                        <span className='text-md font-publicSans tracking-wider font-semibold text-text-color'>
                             aryaswaroop027@gmail.com <br /> aryaswaroop9507@gmail.com
                         </span>
                     </div>
                     <div className='flex items-center mb-2'>
-                        <IoCallSharp className='mr-3 text-xl' />
-                        <span className='text-md font-publicSans tracking-wider font-medium text-gray-900'>+91 8340168072</span>
+                        <IoCallSharp className='mr-3 text-xl text-text-color' />
+                        <span className='text-md font-publicSans tracking-wider font-semibold text-text-color'>+91 8340168072</span>
                     </div>
                 </div>
                 <div className='grid grid-cols-3 gap-5'>
