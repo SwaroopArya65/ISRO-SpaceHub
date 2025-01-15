a3291bc9dd09cbfbce8bc3e627ca966da9ce52ae
Arya Swaroop <aryaswaroop027@gmail.com>
2025-01-15 21:56:30 +0530

    Adding and desgin a text

--- frontend/src/components/Courseslist.jsx
+++ frontend/src/components/Courseslist.jsx
@@ -3,8 +3,11 @@ import courimg from '../assets/sc2.jpg';
 
 const Courseslist = () => {
     return (
-        <div className=''>
-            Courseslist
+        <div className='mt-52'>
+            <div className='flex items-center justify-center flex-col'>
+                <h1 className='font-playfair font-bold tracking-wider text-[2rem] text-text-color2'>Courses</h1>
+                <p className='font-publicSans font-semibold tracking-wider text-xl text-text-color'>Update : - <span className='font-publicSans font-bold tracking-wider text-xl text-text-color'>few sec. ago</span></p>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -9,9 +9,7 @@ const Courses = () => {
                 <Navbar />
             </div>
             <div className='p-6 lg:px-40'>
-                <div className='mt-20 bg-bg2'>
-                    <Courseslist />
-                </div>
+                <Courseslist />
             </div>
         </div>
     )
